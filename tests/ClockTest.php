64e72f46984f784d36cbdb4b9cbcdbe0e3dc428e
Eric Stern <eric@ericstern.com>
2024-03-26 11:50:41 -0700

    test more

--- tests/ClockTest.php
+++ tests/ClockTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Firehed\Clock;
 
 use DateTime;
+use DateInterval;
 use DateTimeImmutable;
+use LogicException;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -74,10 +76,56 @@ class ClockTest extends TestCase
     /**
      * @dataProvider absoluteInputs
      */
-    public function testConstructWithAbsoluteInput($input, DateTimeImmutable $expected): void
+    public function testConstructWithAbsoluteInput(mixed $input, DateTimeImmutable $expected): void
     {
         $clock = new Clock($input);
         self::assertEquals($expected, $clock->now());
+        self::assertNotSame($expected, $clock->now(), 'Should not return same instance from construct');
+    }
+
+    /**
+     * @dataProvider relativeInputs
+     */
+    public function testConstructWithRelativeInput(mixed $input, DateInterval $expectedOffset): void
+    {
+        $now = new DateTimeImmutable();
+        $clock = new Clock($input);
+        $expected = $now->add($expectedOffset);
+
+        self::assertEqualsWithDelta($expected, $clock->now(), 0.001);
+    }
+
+    /**
+     * @dataProvider absoluteInputs
+     */
+    public function testMovingTestClockToAbsoluteTime(mixed $input, DateTimeImmutable $expected): void
+    {
+        $clock = new Clock('now');
+        $beforeMoving = $clock->now();
+        $clock->moveTo($input);
+        self::assertEquals($expected, $clock->now());
+        self::assertNotEquals($beforeMoving, $clock->now());
+    }
+
+    /**
+     * @dataProvider relativeInputs
+     */
+    public function testMovingTestClockWithRelativeInput(mixed $input, DateInterval $expectedOffset): void
+    {
+        $now = new DateTimeImmutable();
+        $clock = new Clock($now);
+        $beforeMoving = $clock->now();
+        $expected = $now->add($expectedOffset);
+        $clock->moveTo($input);
+
+        self::assertEquals($expected, $clock->now());
+    }
+
+    public function testMovingWallClockIsLogicException(): void
+    {
+        $clock = new Clock();
+        self::expectException(LogicException::class);
+        $clock->moveTo('PT1S');
     }
 
     // Provide number as input: should treat as unixtime
@@ -92,7 +140,19 @@ class ClockTest extends TestCase
             'Int timestamp explicit' => [1711475822, new DateTimeImmutable('2024-03-26T17:57:02.000000+0000')],
             // 'Float timestamp' => [1711475822.123456, new DateTimeImmutable('@1711475822.123456')],
             'Float timestamp as @string' => ['@1711475822.123456', new DateTimeImmutable('@1711475822.123456')],
+            'DateTime literal' => [new DateTime('2019-04-12T13:05:27.5216Z'), new DateTimeImmutable('2019-04-12T13:05:27.5216Z')],
+            'DateTimeImmutable literal' => [new DateTimeImmutable('2019-04-12T13:05:27.5216Z'), new DateTimeImmutable('2019-04-12T13:05:27.5216Z')],
 
+        ];
+    }
+
+    public static function relativeInputs(): array
+    {
+        return [
+            'DI literal' => [new DateInterval('P1D'), new DateInterval('P1D')],
+            'DI date string' => ['P1D', new DateInterval('P1D')],
+            'DI time string' => ['PT5S', new DateInterval('PT5S')],
+            'DI date and time string' => ['P5MT5M', new DateInterval('P5MT5M')],
         ];
     }
 }
